982bb777418b169435e4feef21e248c6c35b921c
Brandon Mackey <brandonmackey@fullsail.edu>
2012-09-26 22:05:45 -0400

    TEST commit of MiU folder with added JSON dummy data, search, and search.css

---
+++ Desktop/gh-pages/MiU-project1/JSON.js
@@ -0,0 +1,211 @@
+/*
+Brandon Mackey
+Web App Part 1
+Term 1210
+Sept 26, 2012
+MiU JSON Object for Testing
+*/
+
+var json = {
+    "list1":{
+	"group"	    : ["Type:", "Don't_Forget!"],
+	"importance": ["Priority:", "High Priority" ],
+	"date"	    : ["Date:", "2012-07-17"],
+	"quantity"  : ["Quantity:", "1"],
+	"what"      : ["What:", "BO2"],
+	"where"     : ["Where:", "Gamestop"],
+	"notes"     : ["Notes:", "Black Ops 2"],
+	"favorite"  : ["Favorite:", "Yes"]
+    },
+    "list2":{
+	"group"	    : ["Type:", "Honey_Do_List!"],
+	"importance": ["Priority:", "Low Priority" ],
+	"date"	    : ["Date:", "2012-09-25"],
+	"quantity"  : ["Quantity:", "0"],
+	"what"      : ["What:", "Mow"],
+	"where"     : ["Where:", "My House"],
+	"notes"     : ["Notes:", "Because she said so!"],
+	"favorite"  : ["Favorite:", "No"]
+    },
+    "list3":{
+	"group"	    : ["Type:", "Grocery_List"],
+	"importance": ["Priority:", "High Priority" ],
+	"date"	    : ["Date:", "2012-09-16"],
+	"quantity"  : ["Quantity:", "1"],
+	"what"      : ["What:", "Baby Dipers"],
+	"where"     : ["Where:", "Wal-Mart"],
+	"notes"     : ["Notes:", "Size 3"],
+	"favorite"  : ["Favorite:", "Yes"]
+    },
+    "list4":{
+	"group"	    : ["Type:", "Don't_Forget!"],
+	"importance": ["Priority:", "High Priority" ],
+	"date"	    : ["Date:", "2012-07-25"],
+	"quantity"  : ["Quantity:", "5"],
+	"what"      : ["What:", "gift"],
+	"where"     : ["Where:", "babys R Us"],
+	"notes"     : ["Notes:", "gift for baby"],
+	"favorite"  : ["Favorite:", "Yes"]
+    },
+    "list5":{
+	"group"	    : ["Type:", "Wish_List"],
+	"importance": ["Priority:", "High Priority" ],
+	"date"	    : ["Date:", "2012-12-25"],
+	"quantity"  : ["Quantity:", "1"],
+	"what"      : ["What:", "Xbox 360"],
+	"where"     : ["Where:", "Bestbuy"],
+	"notes"     : ["Notes:", "120GB"],
+	"favorite"  : ["Favorite:", "Yes"]
+    },
+    "list6":{
+	"group"	    : ["Type:", "Don't_Forget!"],
+	"importance": ["Priority:", "High Priority" ],
+	"date"	    : ["Date:", "2012-10-17"],
+	"quantity"  : ["Quantity:", "2"],
+	"what"      : ["What:", "Big Macs"],
+	"where"     : ["Where:", "McDs"],
+	"notes"     : ["Notes:", "Lg Coke"],
+	"favorite"  : ["Favorite:", "Yes"]
+    },
+    "list7":{
+	"group"	    : ["Type:", "Honey_Do_List!"],
+	"importance": ["Priority:", "Low Priority" ],
+	"date"	    : ["Date:", "2012-09-25"],
+	"quantity"  : ["Quantity:", "0"],
+	"what"      : ["What:", "dust"],
+	"where"     : ["Where:", "My House"],
+	"notes"     : ["Notes:", "dusty"],
+	"favorite"  : ["Favorite:", "No"]
+    },
+    "list8":{
+	"group"	    : ["Type:", "Grocery_List"],
+	"importance": ["Priority:", "High Priority" ],
+	"date"	    : ["Date:", "2012-09-16"],
+	"quantity"  : ["Quantity:", "5"],
+	"what"      : ["What:", "Red Peppers"],
+	"where"     : ["Where:", "Wal-Mart"],
+	"notes"     : ["Notes:", "not hot"],
+	"favorite"  : ["Favorite:", "Yes"]
+    },
+    "list9":{
+	"group"	    : ["Type:", "Don't_Forget!"],
+	"importance": ["Priority:", "High Priority" ],
+	"date"	    : ["Date:", "2012-011-25"],
+	"quantity"  : ["Quantity:", "1"],
+	"what"      : ["What:", "b day card"],
+	"where"     : ["Where:", "CVS"],
+	"notes"     : ["Notes:", "bother b day"],
+	"favorite"  : ["Favorite:", "Yes"]
+    },
+    "list10":{
+	"group"	    : ["Type:", "Honey_Do_List!"],
+	"importance": ["Priority:", "Low Priority" ],
+	"date"	    : ["Date:", "2012-09-23"],
+	"quantity"  : ["Quantity:", "0"],
+	"what"      : ["What:", "sweep"],
+	"where"     : ["Where:", "My House"],
+	"notes"     : ["Notes:", "hairy"],
+	"favorite"  : ["Favorite:", "No"]
+    },
+    "list11":{
+	"group"	    : ["Type:", "Grocery_List"],
+	"importance": ["Priority:", "High Priority" ],
+	"date"	    : ["Date:", "2012-10-05"],
+	"quantity"  : ["Quantity:", "1"],
+	"what"      : ["What:", "bread"],
+	"where"     : ["Where:", "Meijer"],
+	"notes"     : ["Notes:", "wheat"],
+	"favorite"  : ["Favorite:", "Yes"]
+    },
+    "list12":{
+	"group"	    : ["Type:", "Grocery_List"],
+	"importance": ["Priority:", "Low Priority" ],
+	"date"	    : ["Date:", "2012-10-05"],
+	"quantity"  : ["Quantity:", "5"],
+	"what"      : ["What:", "apples"],
+	"where"     : ["Where:", "Meijer"],
+	"notes"     : ["Notes:", "green"],
+	"favorite"  : ["Favorite:", "Yes"]
+    },
+    "list13":{
+	"group"	    : ["Type:", "Grocery_List"],
+	"importance": ["Priority:", "High Priority" ],
+	"date"	    : ["Date:", "2012-10-05"],
+	"quantity"  : ["Quantity:", "1"],
+	"what"      : ["What:", "Mac & Cheese"],
+	"where"     : ["Where:", "Meijer"],
+	"notes"     : ["Notes:", "Family size"],
+	"favorite"  : ["Favorite:", "Yes"]
+    },
+    "list14":{
+	"group"	    : ["Type:", "Grocery_List"],
+	"importance": ["Priority:", "High Priority" ],
+	"date"	    : ["Date:", "2012-10-05"],
+	"quantity"  : ["Quantity:", "5"],
+	"what"      : ["What:", "salsa"],
+	"where"     : ["Where:", "Meijer"],
+	"notes"     : ["Notes:", "Mild"],
+	"favorite"  : ["Favorite:", "Yes"]
+    },
+    "list15":{
+	"group"	    : ["Type:", "Grocery_List"],
+	"importance": ["Priority:", "Low Priority" ],
+	"date"	    : ["Date:", "2012-10-05"],
+	"quantity"  : ["Quantity:", "2"],
+	"what"      : ["What:", "chips"],
+	"where"     : ["Where:", "Meijer"],
+	"notes"     : ["Notes:", "salsa chips"],
+	"favorite"  : ["Favorite:", "Yes"]
+    },
+    "list16":{
+	"group"	    : ["Type:", "Grocery_List"],
+	"importance": ["Priority:", "High Priority" ],
+	"date"	    : ["Date:", "2012-10-05"],
+	"quantity"  : ["Quantity:", "2"],
+	"what"      : ["What:", "Pepsi"],
+	"where"     : ["Where:", "Meijer"],
+	"notes"     : ["Notes:", "Next"],
+	"favorite"  : ["Favorite:", "Yes"]
+    },
+    "list17":{
+	"group"	    : ["Type:", "Grocery_List"],
+	"importance": ["Priority:", "Low Priority" ],
+	"date"	    : ["Date:", "2012-10-05"],
+	"quantity"  : ["Quantity:", "1"],
+	"what"      : ["What:", "japs"],
+	"where"     : ["Where:", "Meijer"],
+	"notes"     : ["Notes:", "Hot"],
+	"favorite"  : ["Favorite:", "No"]
+    },
+    "list18":{
+	"group"	    : ["Type:", "Grocery_List"],
+	"importance": ["Priority:", "High Priority" ],
+	"date"	    : ["Date:", "2012-10-05"],
+	"quantity"  : ["Quantity:", "5"],
+	"what"      : ["What:", "green Peppers"],
+	"where"     : ["Where:", "Meijer"],
+	"notes"     : ["Notes:", "Lg"],
+	"favorite"  : ["Favorite:", "Yes"]
+    },
+    "list19":{
+	"group"	    : ["Type:", "Grocery_List"],
+	"importance": ["Priority:", "High Priority" ],
+	"date"	    : ["Date:", "2012-10-05"],
+	"quantity"  : ["Quantity:", "1"],
+	"what"      : ["What:", "cheese"],
+	"where"     : ["Where:", "Meijer"],
+	"notes"     : ["Notes:", "american"],
+	"favorite"  : ["Favorite:", "Yes"]
+    },
+    "list20":{
+	"group"	    : ["Type:", "Grocery_List"],
+	"importance": ["Priority:", "Low Priority" ],
+	"date"	    : ["Date:", "2012-10-05"],
+	"quantity"  : ["Quantity:", "2"],
+	"what"      : ["What:", "watermelon"],
+	"where"     : ["Where:", "Meijer"],
+	"notes"     : ["Notes:", "large"],
+	"favorite"  : ["Favorite:", "No"]
+    }
+}
+
